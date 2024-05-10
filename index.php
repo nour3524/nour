617e94a3914a345f6a97bfb022518ceb3a9d3255
Nour ehab <125605003+nour3524@users.noreply.github.com>
2024-05-10 14:43:07 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -112,13 +112,13 @@
         </div>
         <!-- Carousel End -->
 
-
-        <!-- Facilities Start -->
-        <div class="container-xxl py-5">
+ <!-- Facilities Start -->
+ <div class="container-xxl py-5">
             <div class="container">
                 <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                     <h1 class="mb-3">School Facilities</h1>
                     <p>Discover our tailored school facilities designed to support ADHD children, fostering an environment where they can thrive academically and socially. From specialized classrooms to sensory-friendly spaces, we prioritize their unique needs for optimal learning experiences</p>
+                </div>
                 <div class="row g-4">
                     <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="facility-item">
@@ -128,7 +128,7 @@
                                 <span class="bg-primary"></span>
                             </div>
                             <div class="facility-text bg-primary">
-                                <h3 class="text-primary mb-3">School Bus</h3>
+                                <h3 class="text-primary mb-3"></h3>
                                 <p class="mb-0">Eirmod sed ipsum dolor sit rebum magna erat lorem kasd vero ipsum sit</p>
                             </div>
                         </div>
@@ -176,8 +176,7 @@
             </div>
         </div>
         <!-- Facilities End -->
-
-
+       
         <!-- About Start -->
         <div class="container-xxl py-5">
             <div class="container">
